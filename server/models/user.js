8c67bdb6da1bae42357456b92274f5d585bbc6e6
CakeOfThePans <cary@mobilewareinc.com>
2024-02-05 21:02:13 -0500

    create user model

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import Joi from 'joi'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+export const User = mongoose.model('User', userSchema)
+
+export function validateUser(user){
+    const schema = Joi.object({
+        username: Joi.string().alphanum().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required()
+    })
+
+    return schema.validate(user)
+}
+
+ 
