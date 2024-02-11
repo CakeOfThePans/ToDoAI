91ea9c127012a64f103fa42d25e54a5e8548384d
CakeOfThePans <cary@mobilewareinc.com>
2024-02-11 18:24:10 -0500

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,42 @@
-import { Button, Navbar } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Avatar, Button, Dropdown, Navbar } from "flowbite-react"
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user)
   return (
-    <Navbar className='border-b-2'>
-        <Link to="/" className='self-center whitespace-nowrap text-xl font-semibold'>
-            <div className='px-2 py-1'>
-              ToDoAI
-            </div>
-        </Link>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-xl font-semibold"
+      >
+        <div className="px-2 py-1">ToDoAI</div>
+      </Link>
+      {currentUser ? (
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={<Avatar alt="user" img={currentUser.profilePicture} rounded />}
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">{currentUser.username}</span>
+            <span className="block text-sm font-medium truncate">
+              {currentUser.email}
+            </span>
+          </Dropdown.Header>
+          <Link to={"/profile"}>
+            <Dropdown.Item>Profile</Dropdown.Item>
+          </Link>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign Out</Dropdown.Item>
+        </Dropdown>
+      ) : (
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue" outline>
             Sign In
           </Button>
         </Link>
+      )}
     </Navbar>
   )
 }
